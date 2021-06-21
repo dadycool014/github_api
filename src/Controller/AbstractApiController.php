bdb79d5c24217382cf1d9c5b02a716cab2c22571
mehdi <el_mehdi.ammar@yahoo.com>
2021-06-21 18:40:10 +0100

    Correction of symfony/security-http

--- src/Controller/AbstractApiController.php
+++ src/Controller/AbstractApiController.php
@@ -12,7 +12,6 @@ Abstract class AbstractApiController extends AbstractFOSRestController
 {
     protected function buildForm(string $type , $data=null , array $options = []):FormInterface
     {
-
         $options = array_merge($options,['csrf_protection'=>false,]);
         return $this->container->get('form.factory')->createNamed('',$type,$data,$options);
     }
